831cfd921635af5ec4b0acf47fd89c760d14fe55
Shingo Hiraya <hiraya.shingo@classmethod.jp>
2014-11-19 10:10:19 +0900

    FilterExtentionターゲットを追加

---
+++ FilterExtention/PhotoEditingViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoEditingViewController.h
+//  FilterExtention
+//
+//  Created by hiraya.shingo on 2014/11/19.
+//  Copyright (c) 2014年 hiraya.shingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoEditingViewController : UIViewController
+
+@end
